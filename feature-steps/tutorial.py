99b00fb58f479b96219e1417fe5de9342932b602
Andre Luiz Penha <andrepenha.sistemasenac@gmail.com>
2018-08-29 21:47:14 -0300

    Tutorial Behave

---
+++ feature-steps/tutorial.py
@@ -0,0 +1,30 @@
+from behave import *
+
+@given('we have behave installed')
+def step_impl(context):
+    pass
+
+@when('we implement a test')
+def step_impl(context):
+    assert True is not False
+
+@then('behave will test it for us!')
+def step_impl(context):
+    assert context.failed is False
+
+
+    Run behave:
+
+% behave
+Feature: showing off behave # features/tutorial.feature:1
+
+  Scenario: run a simple test        # features/tutorial.feature:3
+    Given we have behave installed   # features/steps/tutorial.py:3
+    When we implement a test         # features/steps/tutorial.py:7
+    Then behave will test it for us! # features/steps/tutorial.py:11
+
+1 feature passed, 0 failed, 0 skipped
+1 scenario passed, 0 failed, 0 skipped
+3 steps passed, 0 failed, 0 skipped, 0 undefined
+
+Agora, continue lendo para saber como aproveitar ao máximo o behave.
